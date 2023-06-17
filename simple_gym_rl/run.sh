672b42e8bf679704b8714726ac09ee167a444986
dawei <dawei@connect.hku.hk>
2023-06-17 18:50:19 +0800

    add simple RL env dockerfile

---
+++ simple_gym_rl/run.sh
@@ -0,0 +1,7 @@
+docker run -it --gpus all \
+-v $HOME/code/tianshou:/tf/code \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--env="QT_X11_NO_MITSHM=1" \
+--env="DISPLAY" \
+-v $HOME/.Xauthority:/root/.Xauthority \
+wangdawei1996/simple_rl:beta2 bash
