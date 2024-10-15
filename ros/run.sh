bb919eba1fe695363a3d5f936dd86b5af8e8e1f8
dawei <dawei@connect.hku.hk>
2024-10-15 03:56:21 -0600

    add ros dockerfile

---
+++ ros/run.sh
@@ -0,0 +1,12 @@
+docker run -it --gpus all \
+-v $HOME/code/:/code \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--env="QT_X11_NO_MITSHM=1" \
+--env="DISPLAY" \
+--privileged=true \
+-v $HOME/.Xauthority:/root/.Xauthority \
+-v /dev:/dev \
+--device-cgroup-rule "c 81:* rmw" \
+--device-cgroup-rule "c 189:* rmw" \
+--net=host \
+wangdawei1996/rosnoeticfull:norob bash
